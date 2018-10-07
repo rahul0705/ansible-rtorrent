ed04789e72d91da8d8ab972950121dcaf570ea6a
Rahul Mohandas <rahul0705@gmail.com>
2018-10-06 20:07:22 -0400

    develop test(local): added unit tests

--- molecule/travis/tests/test_default.py
+++ molecule/travis/tests/test_default.py
@@ -6,9 +6,20 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_rtorrent_is_installed(host):
+    rtorrent = host.package('rtorrent')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert rtorrent.is_installed
+
+
+def test_rtorrent_running_and_enabled(host):
+    rtorrent = host.service('rtorrent')
+
+    assert rtorrent.is_running
+    assert rtorrent.is_enabled
+
+
+def test_tmux_is_installed(host):
+    tmux = host.package('tmux')
+
+    assert tmux.is_installed
